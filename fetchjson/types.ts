668b0ee742f9dc69e16f9cffc68233fabe55094c
Sadid <sadid.jones@gmail.com>
2023-03-23 22:52:25 +0600

    first commit

---
+++ fetchjson/types.ts
@@ -0,0 +1,22 @@
+// const today = new Date();
+// console.log(today.getDay());
+
+// const person = {
+//   age: 20,
+// };
+
+// class Colour {}
+// const red = new Colour();
+
+let apple: number = 5;
+let orange = "or";
+
+let pointer: { x: number; y: number } = {
+  x: 9,
+  y: 7,
+};
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+logNumber(4);
